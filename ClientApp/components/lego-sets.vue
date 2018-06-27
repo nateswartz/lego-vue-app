9565c6b054d8b68536478dccc12ceffa0235bea8
Nate Swartzentruber <nswartzentruber@gmail.com>
2018-06-27 12:41:56 -0400

    Use a grid of cards to represent sets instead of table

--- ClientApp/components/lego-sets.vue
+++ ClientApp/components/lego-sets.vue
@@ -6,42 +6,37 @@
       <div class="ui text loader">Loading</div>
     </div>
 
-    <select class="ui dropdown" v-model="selectedThemeID">
-      <option v-for="theme in themes" v-bind:value="theme.name">{{ theme.name }}</option>
-    </select>
-    <div class="ui button" @click="filterSets()">Filter</div>
+    <div class="ui segment">
+      <select class="ui dropdown" v-model="selectedThemeID">
+        <option v-for="theme in themes" v-bind:value="theme.name">{{ theme.name }}</option>
+      </select>
+      <div class="ui button" @click="filterSets()">Filter</div>
+    </div>
 
     <template v-if="sets">
+      <div class="ui cards">
+        <div class="ui card" v-for="(set, index) in sets" :key="index">
+          <div class="ui image centered medium">
+            <img v-bind:src="set.imageUrl">
+          </div>
+          <div class="content">
+            <div class="header">{{ set.name }}</div>
+            <div class="meta">
+              <div>{{ set.number }}</div>
+              <a v-bind:href="'/set-parts/' + set.number">{{ set.pieces }} Pieces</a>
+            </div>
+          </div>
+          <div class="extra content">
+            <span class="right floated">
+              {{ set.year }}
+            </span>
+            <span>
+              {{ set.themeName }}
+            </span>
+          </div>
+        </div>
+      </div>
       <table class="ui celled striped table">
-        <thead>
-          <tr>
-            <th>Number</th>
-            <th>Name</th>
-            <th>Year</th>
-            <th>Theme</th>
-            <th>Number of Pieces</th>
-            <th>Image</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(set, index) in sets" :key="index">
-            <td>{{ set.number }}</td>
-            <td>{{ set.name }}</td>
-            <td>{{ set.year }}</td>
-            <td>{{ set.themeName }}</td>
-            <td>
-              <!--TODO: Utilize the router for this instead of a hard-coded link-->
-              <a v-bind:href="'/set-parts/' + set.number">
-                <span>{{ set.pieces }}</span>
-              </a>
-            </td>
-            <td>
-              <a v-bind:href="set.set_url">
-                <img class="ui image tiny" v-bind:src="set.imageUrl" />
-              </a>
-            </td>
-          </tr>
-        </tbody>
         <tfoot>
           <tr>
             <th colspan="3">
